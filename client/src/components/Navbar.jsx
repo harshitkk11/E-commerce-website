17e6d6e3d9973e438303a4dd3863a47f577e305b
Fardin2112 <fardinalam2112@gmail.com>
2023-11-29 10:40:05 +0530

    update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,9 +6,19 @@ const Navbar = () =>{
         <>
         <div className="navbar">
             <div className="nav-logo">
-                <span>E-commerce</span></div>
+                <span>E-commerce</span>
+            </div>
             <div className="nav-items">
                 <ul>
+                    {/* <div className="dropdown">
+                        <button className="dropbtn">Login</button>
+                        <ul>
+                            <li><NavLink>My profile</NavLink></li>
+                            <li><NavLink>Order</NavLink></li>
+                             <li><NavLink>whisList</NavLink></li>
+                             <li><NavLink>Log Out</NavLink></li>
+                         </ul>
+                    </div> */}
                     <li>Cart</li>
                     <li><NavLink to="/login" className="nav-item">Log in</NavLink></li>
                     <li><NavLink to="/signup" className="nav-item">Sign up</NavLink></li>
