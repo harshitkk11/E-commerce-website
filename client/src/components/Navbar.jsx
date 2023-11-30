07b6d116ff49abbdf6730a59d30d41fd3243e28a
harshitkk11 <114001877+harshitkk11@users.noreply.github.com>
2023-11-30 20:12:52 +0530

    nav update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,33 +2,6 @@
 import "../assets/css/Navbar.css";
 import Dropdown from "./Dropdown";
 
-<<<<<<< HEAD
-const Navbar = () =>{
-    return (
-        <>
-        <div className="navbar">
-            <div className="nav-logo">
-                <span>E-commerce</span>
-            </div>
-            <div className="nav-items">
-                <ul>
-                    <li>
-                        <div className="dropdown">
-                           <NavLink className="dropbtn" style={{fontSize:"20px"}} >Login</NavLink>
-                             <div className="dropdown-content">
-                                 <li><NavLink to="/"  style={{textDecoration:"none" ,color:"black"}}>My profile</NavLink></li>
-                                 <li><NavLink to="/"  style={{textDecoration:"none" ,color:"black"}}>Order</NavLink></li>
-                                 <li><NavLink to="/" style={{textDecoration:"none" ,color:"black"}}>whisList</NavLink></li>
-                                 <li><NavLink to="/" style={{textDecoration:"none" ,color:"black"}}>Log Out</NavLink></li>
-                              </div>
-                        </div>
-                    </li>
-                    <li>Cart</li>
-                    <li><NavLink to="/login" className="nav-item">Log in</NavLink></li>
-                    <li><NavLink to="/signup" className="nav-item">Sign up</NavLink></li>
-                </ul>
-            </div>
-=======
 const Navbar = () => {
   const handleMenuOne = () => {
     console.log("Login clicked")
@@ -51,7 +24,6 @@ const Navbar = () => {
       <div className="navbar">
         <div className="nav-logo">
           <span>E-commerce</span>
->>>>>>> 621bb449df8e0c42f2c4b3a4ac50b362b6bf08da
         </div>
         <div className="nav-items">
           <Dropdown
