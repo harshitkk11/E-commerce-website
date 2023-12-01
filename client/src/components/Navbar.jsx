e82d4c61c25cdea2658fe8657597919efadc72d5
Fardin2112 <fardinalam2112@gmail.com>
2023-12-01 17:52:48 +0530

    update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,43 +1,34 @@
-// import { NavLink } from "react-router-dom";
-import "../assets/css/Navbar.css";
-import Dropdown from "./Dropdown";
+import { NavLink } from "react-router-dom"
+import "../assets/css/Navbar.css"
+import Searchbar from "./Searchbar";
+import { VscAccount } from "react-icons/vsc";
+import { RiShoppingCartFill } from "react-icons/ri";
 
 const Navbar = () => {
-  const handleMenuOne = () => {
-    console.log("Login clicked")
-  };
-
-  const handleMenuTwo = () => {
-    console.log("My Profile clicked")
-  };
-
-  const handleMenuThree = () => {
-    console.log("Order clicked")
-  };
-
-  const handleMenuFour = () => {
-    console.log("whisList clicked")
-  };
-
-  return (
-    <>
-      <div className="navbar">
-        <div className="nav-logo">
-          <span>E-commerce</span>
+    return (
+        <>
+        <div className="navbar">
+            <div className="nav-logo">
+                <span>E-commerce</span>
+                <Searchbar/>
+            </div>
+            <div className="nav-items">
+                        <div className="dropdown">
+                           <button className="dropbtn"> <VscAccount/> Login</button>
+                             <div className="dropdown-content">
+                                 <li><NavLink to="/login"  style={{textDecoration:"none" ,color:"black"}}>Login</NavLink></li>
+                                 <li><NavLink to="/"  style={{textDecoration:"none" ,color:"black"}}>My profile</NavLink></li>
+                                 <li><NavLink to="/"  style={{textDecoration:"none" ,color:"black"}}>Order</NavLink></li>
+                                 <li><NavLink to="/" style={{textDecoration:"none" ,color:"black"}}>whisList</NavLink></li>
+                                 <li><NavLink to="/" style={{textDecoration:"none" ,color:"black"}}>Log Out</NavLink></li>
+                              </div>
+                        </div>
+                        <div className="Cart">
+                            <button> Cart <RiShoppingCartFill /> </button>
+                        </div>
+            </div>
         </div>
-        <div className="nav-items">
-          <Dropdown
-            trigger={<button className="dropdown-button">Log in</button>}
-            menu={[
-                <button onClick={handleMenuOne}>Log in</button>,
-                <button onClick={handleMenuTwo}>My Profile</button>,
-                <button onClick={handleMenuThree}>Order</button>,
-                <button onClick={handleMenuFour}>whisList</button>
-            ]}
-          />
-        </div>
-      </div>
-    </>
-  );
-};
+        </>
+    )
+}
 export default Navbar;
---
+++ client/src/components/Searchbar.jsx
@@ -0,0 +1,15 @@
+
+
+const Searchbar = () => {
+
+    return (
+       <div className="Searchbar-nav">
+        <input 
+            type="search"
+            placeholder='Search for Products'
+            />  
+       </div> 
+     
+    )
+}
+export default Searchbar;
