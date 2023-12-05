1ad48c29309354a117ffebb38124e4131b3a7c40
Fardin2112 <fardinalam2112@gmail.com>
2023-12-05 22:37:59 +0530

    work on product_structure

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,25 @@
+import Product_struc from "./Product_struc";
+import Ads from "./Ads";
+import "../assets/css/Home.css"
+
 function Home() {
+
+    // const Data = async () => {
+    //     const url = "../data/fashion.csvv";
+    //     setLoading(true )
+    //     let data = await fetch(url);
+    //     let parsedData = await data.json();
+    //     setArticles(parsedData.articles);
+    //     setLoading(false);
+    //     settotalResults(parsedData.totalResults);
+    // }
     return (
         <div className="home">
-           <h1>Home</h1>
-            
-        </div>
+            <Ads/>
+            <div className="products-container">
+                <Product_struc/>
+            </div>
+        </div>      
     )
 }
 export default Home;
