15e06792c72ed87a89222a8a4b76a461ff90162e
Fardin2112 <fardinalam2112@gmail.com>
2023-12-19 00:26:55 +0530

    working on footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,25 +1,55 @@
-import "../assets/css/Footer.css"
+import "../assets/css/Footer.css";
 
-function Footer (){
-    return(
-        <div className="footer">
-            <div className="about">
-                <span>ABOUT</span>
-                <nav className="about-nav">
-                    <li>Contact Us</li>
-                    <li>About us</li>
-                    <li>Carrer</li>
-                </nav>
-            </div>
-            <div className="help">
-                <h3>Let Us Help You</h3>
-                <nav className="help-nav">
-                    <li>Payment</li>
-                    <li>Shipping</li>
-                    <li>Cancelation & return</li>
-                </nav>
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-left">
+        <div className="about">
+          <span>ABOUT</span>
+          <nav className="nav">
+            <li>Contact Us</li>
+            <li>About us</li>
+            <li>Carrer</li>
+          </nav>
+        </div>
+        <div className="help">
+          <span>Let Us Help You</span>
+          <nav className="nav">
+            <li>Payment</li>
+            <li>Shipping</li>
+            <li>Cancelation & return</li>
+          </nav>
+        </div>
+        <div className="consumer">
+          <span>Consumer Policy</span>
+          <nav className="nav">
+            <li>Term and Use</li>
+            <li>Security</li>
+            <li>Privacy</li>
+          </nav>
+        </div>
+        <div className="social">
+          <span>Social</span>
+          <nav className="nav">
+            <li>Instagram</li>
+            <li>Facebook</li>
+            <li>YouTube</li>
+          </nav>
+        </div>
+      </div>
+      <div className="footer-right">
+        <div className="Mail">
+          <span>Mail Us</span>
+        </div>
+        <div className="register">
+            <div className="add">
+               <span>Register offcie Address</span>
+               <p>Knowledge Park 2</p>
+               <p>Greater Noida , Uttar Pardesh</p>
             </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 export default Footer;
